8079039ee8d0284c6636e4988c106fb536a8f571
Alex Levanov <ilewanof@gmail.com>
2018-02-07 13:00:28 +0400

    Add entry point in application

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import Button from 'components/Button';
+
+const App = () => (
+  <div><Button /></div>
+);
+
+export default hot(module)(App);
