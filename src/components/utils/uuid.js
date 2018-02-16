940f1924dced879646321194a9941e5be049cbaa
Alex Levanov <ilewanof@gmail.com>
2018-02-16 18:17:16 +0400

    Add utils for ui-components

---
+++ src/components/utils/uuid.js
@@ -0,0 +1,15 @@
+let i = 0;
+let now = 0;
+
+const prefix = Math.random()
+  .toString(36)
+  .slice(2);
+
+export default function uuid() {
+  const currentNow = Date.now();
+  if (currentNow > now) {
+    now = currentNow;
+    i = 0;
+  }
+  return `${prefix}-${now}-${i + 1}`;
+}
